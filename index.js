fdba0e993828369b2e8b24f62666b221ba0b71a9
NinjaCoder0516 <haotian.song.0516@gmail.com>
2023-09-03 21:26:55 +0800

    DONE

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const openai = new OpenAI({
 async function main() {
   const completion = await openai.chat.completions.create({
     messages: [{ role: "user", content: "hello" }],
-    model: "davinci-002",
+    model: "gpt-3.5-turbo",
   });
 
-  console.log(completion.choices[0]);
+  console.log(completion?.choices[0]?.message?.content);
 }
 
 main();
